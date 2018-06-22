5a8b49a22591e886c9df3eb357d48077cf2bc937
Czapel <czapelxd@gmail.com>
2018-06-22 13:39:55 +0200

    add react props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import PropTypes from 'prop-types';
 
+const App = (props) => {
+    return (
+        <h2 className={"text-center"}>
+            {props.headerMassage}
+        </h2>
+    );
+};
+
+App.propTypes = {
+    headerMassage: PropTypes.string
+};
+
+App.defaultProps = {
+    headerMassage: 'Hello!'
+};
 
 ReactDOM.render(
-    <h2 className="text-center">
-        Hello React with JSX!! -- {Math.random()}
-    </h2>,
+    <App />,
     document.getElementById('root')
 )
